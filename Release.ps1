cafd946515612378bb61631f8c2ec5b8e78c1703
stax76 <frank.skare.de@gmail.com>
2020-09-22 19:10:46 +0200

    release script

--- Release.ps1
+++ Release.ps1
@@ -6,3 +6,7 @@ Copy-Item .\MediaInfoSharp.dll "$targetDir\MediaInfoSharp.dll"
 Copy-Item .\MediaInfoSharp.pdb "$targetDir\MediaInfoSharp.pdb"
 Copy-Item .\Get-MediaInfo.psd1 "$targetDir\Get-MediaInfo.psd1"
 Copy-Item .\Get-MediaInfo.psm1 "$targetDir\Get-MediaInfo.psm1"
+
+$key = Read-Host 'Enter Api Key'
+
+Publish-Module -Path $targetDir -NuGetApiKey $key
